c0fc50ae4126b0fd0ea515caccd5bbef4c14e1fa
flowerfrog <alexosenn@gmail.com>
2023-12-07 15:32:29 +0300

    Added test edit card

---
+++ api_tests/test_edit_card.py
@@ -0,0 +1,47 @@
+import jsonschema
+import requests
+from api_tests.utils import load_schema, load_env
+
+
+def test_edit_card_title():
+    card_id = 1314861
+    card_title = 'test_change_name_card'
+    API_KEY = load_env()
+    url = f"https://private.mybrocard.com/api/v2/cards/{card_id}"
+    schema = load_schema('cards/put_edit_card.json')
+
+    result = requests.put(url,
+                          headers={
+                              "Authorization": f'Bearer {API_KEY}',
+                              "Content-Type": "application/json",
+                              "Accept": "application/json"
+                          },
+                          params={
+                              "title": card_title
+                          })
+
+    assert result.status_code == 200
+    jsonschema.validate(result.json(), schema)
+    assert result.json()['title'] == card_title
+
+
+def test_edit_card_owner():
+    card_id = 1314861
+    user_id = 4
+    API_KEY = load_env()
+    url = f"https://private.mybrocard.com/api/v2/cards/{card_id}"
+    schema = load_schema('cards/put_edit_card.json')
+
+    result = requests.put(url,
+                          headers={
+                              "Authorization": f'Bearer {API_KEY}',
+                              "Content-Type": "application/json",
+                              "Accept": "application/json"
+                          },
+                          params={
+                              "user_id": user_id
+                          })
+
+    assert result.status_code == 200
+    jsonschema.validate(result.json(), schema)
+    assert result.json()['user']['id'] == user_id
