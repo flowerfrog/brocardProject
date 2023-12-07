bcec5a416ab3c02ca1ebea96f6094c00f9d8756d
flowerfrog <alexosenn@gmail.com>
2023-12-07 11:29:49 +0300

    Added test for get card logs non-company members

--- api_tests/test_get_card_logs.py
+++ api_tests/test_get_card_logs.py
@@ -21,6 +21,24 @@ def test_get_card_logs_company_member():
     assert result.json()['data'][0]['card']['id'] == card_id
 
 
+def test_get_card_logs_non_company_member():
+    card_id = 1324141
+    API_KEY = load_env()
+    url = f"https://private.mybrocard.com/api/v2/cards/{card_id}/log"
+    schema = load_schema('cards/get_card_logs_non_company_member.json')
+
+    result = requests.get(url,
+                          headers={
+                              "Authorization": f'Bearer {API_KEY}',
+                              "Content-Type": "application/json",
+                              "Accept": "application/json"
+                          })
+
+    assert result.status_code == 200
+    jsonschema.validate(result.json(), schema)
+    assert result.json()['data'] == []
+
+
 def test_list_of_cards_per_page():
     card_id = 343604
     page = 2
