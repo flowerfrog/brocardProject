72f1a8142a84d52af6552f0bdec275ad4dbb4dc9
flowerfrog <alexosenn@gmail.com>
2023-12-19 09:38:49 +0300

    Added test for getting company bins

---
+++ api_tests/test_get_company_bins.py
@@ -0,0 +1,20 @@
+import jsonschema
+from api_tests.utils import load_schema, load_env
+from api_tests.utils import brocard_api_get
+
+
+def test_get_company_bins():
+    API_KEY = load_env()
+    url = "/company/bins"
+    schema = load_schema('company/get_company_bins.json')
+
+    result = brocard_api_get(url,
+                             headers={
+                                 "Authorization": f'Bearer {API_KEY}',
+                                 "Content-Type": "application/json",
+                                 "Accept": "application/json"
+                             })
+
+    assert result.status_code == 200
+    jsonschema.validate(result.json(), schema)
+    assert result.json()['total'] == 36
