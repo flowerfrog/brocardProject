1d058070cc14a8cddb2fe6c1a131a820a709dfd5
flowerfrog <alexosenn@gmail.com>
2023-12-19 13:13:17 +0300

    Added tests for getting data of payments

---
+++ api_tests/test_show_payments.py
@@ -0,0 +1,48 @@
+import allure
+import jsonschema
+from api_tests.utils import load_schema, load_env
+from api_tests.utils import brocard_api_get
+
+
+@allure.tag("api")
+@allure.label("owner", "flowerfrog")
+@allure.feature('API')
+@allure.story('Getting a payment with a given id')
+def test_get_payment_of_company_member_with_given_id():
+    API_KEY = load_env()
+    payment_id = 31393250
+    url = f"/payments/{payment_id}"
+    schema = load_schema('payments/get_show_payment.json')
+
+    result = brocard_api_get(url,
+                             headers={
+                                 "Authorization": f'Bearer {API_KEY}',
+                                 "Content-Type": "application/json",
+                                 "Accept": "application/json"
+                             })
+
+    assert result.status_code == 200
+    jsonschema.validate(result.json(), schema)
+    assert result.json()['id'] == 31393250
+
+
+@allure.tag("api")
+@allure.label("owner", "flowerfrog")
+@allure.feature('API')
+@allure.story('Getting a payment with a given id')
+def test_get_payment_of_non_company_member_with_given_id():
+    API_KEY = load_env()
+    payment_id = 31393251
+    url = f"/payments/{payment_id}"
+    schema = load_schema('payments/get_show_payments_non_company_member.json')
+
+    result = brocard_api_get(url,
+                             headers={
+                                 "Authorization": f'Bearer {API_KEY}',
+                                 "Content-Type": "application/json",
+                                 "Accept": "application/json"
+                             })
+
+    assert result.status_code == 403
+    jsonschema.validate(result.json(), schema)
+    assert result.json()['message'] == "This action is unauthorized."
