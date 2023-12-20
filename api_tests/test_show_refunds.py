a070226ee22e1fae94303cac26256ed20fb19d66
flowerfrog <alexosenn@gmail.com>
2023-12-20 11:02:37 +0300

    Added test for showing data of refund with id

---
+++ api_tests/test_show_refunds.py
@@ -0,0 +1,49 @@
+import allure
+import jsonschema
+from api_tests.utils import load_schema, load_env
+from api_tests.utils import brocard_api_get
+
+
+@allure.tag("api")
+@allure.label("owner", "flowerfrog")
+@allure.feature('API')
+@allure.story('Getting a refund with a given id')
+def test_get_refund_of_company_member_with_given_id():
+    API_KEY = load_env()
+    refund_id = 80078
+    url = f"/refunds/{refund_id}"
+    schema = load_schema('refunds/get_list_refunds.json')
+
+    result = brocard_api_get(url,
+                             headers={
+                                 "Authorization": f'Bearer {API_KEY}',
+                                 "Content-Type": "application/json",
+                                 "Accept": "application/json"
+                             })
+
+    assert result.status_code == 200
+    jsonschema.validate(result.json(), schema)
+    assert result.json()['data'][0]['id'] == 80078
+    assert len(result.json()['data']) == 1
+
+
+@allure.tag("api")
+@allure.label("owner", "flowerfrog")
+@allure.feature('API')
+@allure.story('Getting a refund with a given id')
+def test_get_refund_of_non_company_member_with_given_id():
+    API_KEY = load_env()
+    refund_id = 92976
+    url = f"/refunds/{refund_id}"
+    schema = load_schema('refunds/get_show_refund_non_company_member.json')
+
+    result = brocard_api_get(url,
+                             headers={
+                                 "Authorization": f'Bearer {API_KEY}',
+                                 "Content-Type": "application/json",
+                                 "Accept": "application/json"
+                             })
+
+    assert result.status_code == 403
+    jsonschema.validate(result.json(), schema)
+    assert result.json()['message'] == "This action is unauthorized."
