56399013ddbb2a1e1f965185b9e800abd8ffe87a
flowerfrog <alexosenn@gmail.com>
2024-01-17 16:07:55 +0300

    Added test for check authorized registered customer

--- brocard_project/pages/main_page.py
+++ brocard_project/pages/main_page.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 from selene import browser, be, have
 
 
@@ -6,14 +7,22 @@ class MainPage:
         browser.open("login")
         return self
 
+    def set_cookie_authorized_without_2fa(self, cookie_name, cookie_value):
+        browser.driver.add_cookie(dict(name=cookie_name, value=cookie_value))
+        return self
+
     def filling_authorization_form(self, user):
         browser.element('[id="email"]').should(be.visible).type(user.email)
         browser.element('[id="password"]').should(be.visible).type(user.password)
         browser.element('[type="submit"]').should(be.visible).click()
         return self
 
-    def user_must_be_authorized(self, user):
+    def admin_must_be_authorized(self, user):
         browser.element('.email.text-muted').should(have.text(user.email))
+        return self
+
+    def customer_must_be_authorized(self, user):
+        browser.element('[href="https://private.mybrocard.com/profile"] > div > .sub').should(have.text(user.email))
         return self
 
 
--- tests/web_tests/user_authorization_test.py
+++ tests/web_tests/user_authorization_test.py
@@ -26,4 +26,32 @@ def test_authorization_registered_admin():
         main_page.filling_authorization_form(user)
 
     with allure.step("Checking that user has been authorized"):
-        main_page.user_must_be_authorized(user)
+        main_page.admin_must_be_authorized(user)
+
+
+@allure.epic('Authorized')
+@allure.label("owner", "flowerfrog")
+@allure.feature("Checking the authorization of the user")
+@allure.label('microservice', 'WEB')
+@allure.tag('regress', 'web', 'normal')
+@allure.severity('normal')
+@allure.label('layer', 'web')
+def test_authorization_registered_customer():
+
+    user = User(
+        name=os.getenv('CUSTOMER_NAME'),
+        email=os.getenv('CUSTOMER_EMAIL'),
+        password=os.getenv('CUSTOMER_PASSWORD')
+    )
+
+    with allure.step("Open the main page"):
+        main_page.open()
+
+    with allure.step("Set cookies for authorized without 2FA"):
+        main_page.set_cookie_authorized_without_2fa(cookie_name=os.getenv('COOKIE_NAME'), cookie_value=os.getenv('COOKIE_VALUE'))
+
+    with allure.step("Filling the authorization form"):
+        main_page.filling_authorization_form(user)
+
+    with allure.step("Checking that user has been authorized"):
+        main_page.customer_must_be_authorized(user)
