06d68b95c88123728b741ec75057c9b766dee8bd
flowerfrog <alexosenn@gmail.com>
2024-01-31 10:12:42 +0300

    Added test for checking statistics in widget teams

--- tests/web_tests/test_stats_in_company_dashboard.py
+++ tests/web_tests/test_stats_in_company_dashboard.py
@@ -3,6 +3,7 @@ import allure
 
 from brocard_project.data.companies import Company
 from brocard_project.data.users import User
+from brocard_project.data.teams import Team
 from brocard_project.pages.main_page import main_page
 from brocard_project.pages.company_dashboard import company_dashboard
 from brocard_project.pages.card_page import card_page
@@ -10,6 +11,8 @@ from brocard_project.pages.payments_page import payment_page
 from brocard_project.pages.profile_page import profile_page
 from brocard_project.pages.admin_companies_page import companies_page
 from brocard_project.pages.admin_user_profile import user_profile
+from brocard_project.pages.members_page import members_page
+from brocard_project.pages.admin_teams_page import teams_page
 
 
 @allure.epic('Statistics')
@@ -20,7 +23,6 @@ from brocard_project.pages.admin_user_profile import user_profile
 @allure.severity('normal')
 @allure.label('layer', 'web')
 def test_stats_in_widget_active_card_of_company():
-
     user = User(
         id=0,
         name=os.getenv('CUSTOMER_NAME'),
@@ -65,7 +67,6 @@ def test_stats_in_widget_active_card_of_company():
 @allure.severity('normal')
 @allure.label('layer', 'web')
 def test_stats_in_widget_members():
-
     user = User(
         id=0,
         name=os.getenv('CUSTOMER_NAME'),
@@ -108,7 +109,6 @@ def test_stats_in_widget_members():
 @allure.severity('normal')
 @allure.label('layer', 'web')
 def test_stats_in_widget_released_cards_today():
-
     user = User(
         id=0,
         name=os.getenv('CUSTOMER_NAME'),
@@ -151,7 +151,6 @@ def test_stats_in_widget_released_cards_today():
 @allure.severity('normal')
 @allure.label('layer', 'web')
 def test_stats_in_widget_cashback():
-
     user = User(
         id=0,
         name=os.getenv('CUSTOMER_NAME'),
@@ -198,7 +197,6 @@ def test_stats_in_widget_cashback():
 @allure.severity('normal')
 @allure.label('layer', 'web')
 def test_stats_in_widget_company_payments_for_30_days():
-
     user = User(
         id=0,
         name=os.getenv('CUSTOMER_NAME'),
@@ -241,7 +239,6 @@ def test_stats_in_widget_company_payments_for_30_days():
 @allure.severity('normal')
 @allure.label('layer', 'web')
 def test_stats_in_widget_decline_rate():
-
     user = User(
         id=34071,
         name=os.getenv('CUSTOMER_NAME'),
@@ -286,3 +283,64 @@ def test_stats_in_widget_decline_rate():
                      "with the DR values of the selected company in the companies section"):
         companies_page.choosing_company_in_the_filter_and_applying_the_filter(company)
         companies_page.get_values_DR7_and_DR30_from_companies(company)
+
+
+@allure.epic('Statistics')
+@allure.label("owner", "flowerfrog")
+@allure.feature("Checking the display of statistics in the widget TEAMS")
+@allure.label('microservice', 'WEB')
+@allure.tag('regress', 'web', 'normal')
+@allure.severity('normal')
+@allure.label('layer', 'web')
+def test_stats_in_widget_teams():
+    user = User(
+        id=2,
+        name=os.getenv('CUSTOMER_NAME'),
+        email=os.getenv('CUSTOMER_EMAIL'),
+        password=os.getenv('CUSTOMER_PASSWORD')
+    )
+
+    admin = User(
+        id=0,
+        name=os.getenv('ADMIN_NAME'),
+        email=os.getenv('ADMIN_EMAIL'),
+        password=os.getenv('ADMIN_PASSWORD')
+    )
+
+    team = Team(
+        id=1,
+        name='Малинки',
+        count_members='21',
+        count_active_card='38',
+        spend='$237.06',
+        total_balance='$1,449.79',
+        dr_7='-',
+        dr_30='-'
+    )
+
+    with allure.step("Open the company dashboard"):
+        main_page.open()
+        main_page.filling_authorization_form(admin)
+        user_profile.open(user)
+        user_profile.login_as(user)
+
+    with allure.step("Get the values of the widget TEAMS"):
+        company_dashboard.get_values_from_widget_teams(team)
+
+    with allure.step("Open the members page in the user cabinet"):
+        company_dashboard.clicking_on_link_in_column_members_in_widget_members()
+
+    with allure.step("Checking that the values on the company dashboard converge "
+                     "with the values of the selected team in the members section"):
+        # members_page.get_values_total_balance_and_spend(team)
+        members_page.get_count_members_of_team(team)
+
+    with allure.step("Open the teams page in the admin cabinet"):
+        profile_page.open()
+        profile_page.stop_impersonate()
+        teams_page.open()
+
+    with allure.step("Checking that the values on the company dashboard converge "
+                     "with the values of the selected team in the teams section"):
+        teams_page.choosing_team_in_the_filter_and_applying_the_filter(team)
+        teams_page.get_values_DR7_and_DR30_from_teams(team)
