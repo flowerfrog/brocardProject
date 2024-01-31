afe11b5c2daafd123f2c622ad8f46376fa8f00fe
flowerfrog <alexosenn@gmail.com>
2024-01-31 10:11:32 +0300

    Added dataclass teams

---
+++ brocard_project/data/teams.py
@@ -0,0 +1,13 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class Team:
+    id: int
+    name: str
+    count_members: str
+    count_active_card: str
+    spend: str
+    total_balance: str
+    dr_7: str
+    dr_30: str
